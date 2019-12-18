63646bc2bcdb8217171b9d351e62a7ddf60a8ad6
Paul Cosma <paul.cosma97@gmail.com>
2019-12-18 10:29:43 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
 cd /root/minijobs
 
-# git clean -d -x -f
-git pull
+if [ "$1" != "--phase-2" ]; then
+    git pull
+    bash ./deploy.sh --phase-2
+    exit 0
+fi
 
 export MJ_PROD=true
 export MJ_PORT=443
@@ -23,4 +28,5 @@ npx prockill -p 443 -k
 npx wait-port 50500 # waits for db
 
 nohup npm start > "../logs/server-$(date "+%Y-%m-%dT%H:%M:%S").log"
+npx wait-port -t 30000 443
 exit 0
