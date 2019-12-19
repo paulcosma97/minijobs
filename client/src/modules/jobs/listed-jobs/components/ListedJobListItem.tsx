1c41d774739c70620715459b4dec3fe5197e23f9
Paul Cosma <paul.cosma97@gmail.com>
2019-12-19 13:44:50 +0200

    Improves listed job card style

--- client/src/modules/jobs/listed-jobs/components/ListedJobListItem.tsx
+++ client/src/modules/jobs/listed-jobs/components/ListedJobListItem.tsx
@@ -11,23 +11,18 @@ export interface ListedJobListItemProps {
 const ListedJobListItem: FC<ListedJobListItemProps> = ({ listedJob }) => {
     return (
         <div className="listed-job-card">
-            <div className="listed-job-card-header">
-                <h3>Voi {listedJob.name}</h3>
-            </div>
-
             <img src={env.baseUrl + listedJob.pictures[0]} alt={listedJob.name} />
-            <Avatar src={env.baseUrl + listedJob.user.picture} />
-            <span>{listedJob.user.firstName + ' ' + listedJob.user.lastName}</span>
 
             <div className="listed-job-card-container">
-
+                <h3>Voi {listedJob.name}</h3>
 
                 <p className="listed-job-card-description">{listedJob.description}</p>
+                <div style={{ textAlign: 'right' }}>{listedJob.user.firstName + ' ' + listedJob.user.lastName}</div>
 
                 <div className="price-list">
-                    <span><Icon type="shopping" />{listedJob.packages[0].price} - {listedJob.packages.pop().price} RON</span>
-                    <span><Icon type="eye" /> {listedJob.views} Viz.</span>
-                    <span><Icon type="star" /> {listedJob.computedRating || '??'} (17)</span>
+                    <div><Icon type="shopping" />{listedJob.packages[0].price} - {listedJob.packages.pop().price} RON</div>
+                    <div><Icon type="eye" /> {listedJob.views} Viz.</div>
+                    <div><Icon type="star" /> {listedJob.computedRating || '??'} (17)</div>
                 </div>
             </div>
         </div>
